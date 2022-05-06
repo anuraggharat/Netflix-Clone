116d8cbce5fe748150a5ab5410d6fa70a5d83b82
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2022-05-06 16:05:30 +0530

    plan page added

--- pages/signup/steptwoplan.js
+++ pages/signup/steptwoplan.js
@@ -2,10 +2,16 @@ import React,{useState} from 'react'
 import { BsPhone, BsTablet , } from "react-icons/bs";
 import {HiOutlineDesktopComputer} from 'react-icons/hi'
 import {FiMonitor} from 'react-icons/fi'
+import Link from "next/link";
+
 
 export default function Steptwoplan() {
 
 const[selected,setSelected]=useState("basic");
+
+const changePlan=(sel)=>{
+    setSelected(sel)
+}
 
   return (
     <div className="py-40 z-0">
@@ -68,7 +74,10 @@ const[selected,setSelected]=useState("basic");
         <div className="container mt-10">
           <div className="w-100 flex">
             <div className="w-2/6 p-0"></div>
-            <div className="w-1/6  flex flex-col justify-center  text-center items-center">
+            <div
+              className="w-1/6  flex flex-col justify-center  text-center items-center cursor-pointer"
+              onClick={() => changePlan("mobile")}
+            >
               <div
                 className={
                   "w-28 h-28 font-normal text-xl flex rounded-sm flex-col justify-center  items-center bg-[#e50914] " +
@@ -78,7 +87,10 @@ const[selected,setSelected]=useState("basic");
                 <p>Mobile</p>
               </div>
             </div>
-            <div className="w-1/6 p-2 flex flex-col justify-center  text-center items-center">
+            <div
+              className="w-1/6 p-2 flex flex-col justify-center  text-center items-center cursor-pointer"
+              onClick={() => changePlan("basic")}
+            >
               <div
                 className={
                   "w-28 h-28 font-normal text-xl flex rounded-sm flex-col justify-center  items-center bg-[#e50914] " +
@@ -88,7 +100,10 @@ const[selected,setSelected]=useState("basic");
                 <p>Basic</p>
               </div>
             </div>
-            <div className="w-1/6 p-2 flex flex-col justify-center    text-center items-center">
+            <div
+              className="w-1/6 p-2 flex flex-col justify-center    text-center items-center cursor-pointer"
+              onClick={() => changePlan("standard")}
+            >
               <div
                 className={
                   "w-28 h-28 font-normal text-xl flex rounded-sm flex-col justify-center  items-center bg-[#e50914] " +
@@ -98,7 +113,10 @@ const[selected,setSelected]=useState("basic");
                 <p>Standard</p>
               </div>
             </div>
-            <div className="w-1/6 p-2 flex flex-col justify-center  text-center items-center">
+            <div
+              className="w-1/6 p-2 flex flex-col justify-center  text-center items-center cursor-pointer"
+              onClick={() => changePlan("premium")}
+            >
               <div
                 className={
                   "w-28 h-28 font-normal text-xl flex rounded-sm flex-col justify-center  items-center bg-[#e50914] " +
@@ -114,37 +132,25 @@ const[selected,setSelected]=useState("basic");
               <p>Monthly Price</p>
             </div>
             <div className="w-1/6  flex flex-col justify-center  text-center items-center">
+              <p className={selected == "mobile" ? "text-white" : "opacity-40"}>
+                ₹ 149/-
+              </p>
+            </div>
+            <div className="w-1/6  flex flex-col justify-center  text-center items-center">
+              <p className={selected == "basic" ? "text-white" : "opacity-40"}>
+                ₹ 149/-
+              </p>
+            </div>
+            <div className="w-1/6  flex flex-col justify-center  text-center items-center">
               <p
-                className={
-                  selected == "mobile" ? "text-[#e50914]" : "opacity-40"
-                }
+                className={selected == "standard" ? "text-white" : "opacity-40"}
               >
                 ₹ 149/-
               </p>
             </div>
             <div className="w-1/6  flex flex-col justify-center  text-center items-center">
               <p
-                className={
-                  selected == "basic" ? "text-[#e50914]" : "opacity-40"
-                }
-              >
-                ₹ 149/-
-              </p>
-            </div>
-            <div className="w-1/6  flex flex-col justify-center  text-center items-center">
-              <p
-                className={
-                  selected == "standard" ? "text-[#e50914]" : "opacity-40"
-                }
-              >
-                ₹ 149/-
-              </p>
-            </div>
-            <div className="w-1/6  flex flex-col justify-center  text-center items-center">
-              <p
-                className={
-                  selected == "premium" ? "text-[#e50914]" : "opacity-40"
-                }
+                className={selected == "premium" ? "text-white" : "opacity-40"}
               >
                 ₹ 149/-
               </p>
@@ -156,37 +162,25 @@ const[selected,setSelected]=useState("basic");
               <p>Video Quality</p>
             </div>
             <div className="w-1/6  flex flex-col justify-center  text-center items-center">
-              <p
-                className={
-                  selected == "mobile" ? "text-[#e50914]" : "opacity-40"
-                }
-              >
+              <p className={selected == "mobile" ? "text-white" : "opacity-40"}>
+                Good
+              </p>
+            </div>
+            <div className="w-1/6  flex flex-col justify-center  text-center items-center">
+              <p className={selected == "basic" ? "text-white" : "opacity-40"}>
                 Good
               </p>
             </div>
             <div className="w-1/6  flex flex-col justify-center  text-center items-center">
               <p
-                className={
-                  selected == "basic" ? "text-[#e50914]" : "opacity-40"
-                }
-              >
-                Good
-              </p>
-            </div>
-            <div className="w-1/6  flex flex-col justify-center  text-center items-center">
-              <p
-                className={
-                  selected == "standard" ? "text-[#e50914]" : "opacity-40"
-                }
+                className={selected == "standard" ? "text-white" : "opacity-40"}
               >
                 Better
               </p>
             </div>
             <div className="w-1/6  flex flex-col justify-center  text-center items-center">
               <p
-                className={
-                  selected == "premium" ? "text-[#e50914]" : "opacity-40"
-                }
+                className={selected == "premium" ? "text-white" : "opacity-40"}
               >
                 Best
               </p>
@@ -198,37 +192,25 @@ const[selected,setSelected]=useState("basic");
               <p>Resolution</p>
             </div>
             <div className="w-1/6  flex flex-col justify-center  text-center items-center">
-              <p
-                className={
-                  selected == "mobile" ? "text-[#e50914]" : "opacity-40"
-                }
-              >
+              <p className={selected == "mobile" ? "text-white" : "opacity-40"}>
+                480p
+              </p>
+            </div>
+            <div className="w-1/6  flex flex-col justify-center  text-center items-center">
+              <p className={selected == "basic" ? "text-white" : "opacity-40"}>
                 480p
               </p>
             </div>
             <div className="w-1/6  flex flex-col justify-center  text-center items-center">
               <p
-                className={
-                  selected == "basic" ? "text-[#e50914]" : "opacity-40"
-                }
-              >
-                480p
-              </p>
-            </div>
-            <div className="w-1/6  flex flex-col justify-center  text-center items-center">
-              <p
-                className={
-                  selected == "standard" ? "text-[#e50914]" : "opacity-40"
-                }
+                className={selected == "standard" ? "text-white" : "opacity-40"}
               >
                 1080p
               </p>
             </div>
             <div className="w-1/6  flex flex-col justify-center  text-center items-center">
               <p
-                className={
-                  selected == "premium" ? "text-[#e50914]" : "opacity-40"
-                }
+                className={selected == "premium" ? "text-white" : "opacity-40"}
               >
                 4K + HDR
               </p>
@@ -242,7 +224,7 @@ const[selected,setSelected]=useState("basic");
             <div
               className={
                 "w-1/6  flex flex-col justify-start  text-center items-center " +
-                (selected == "mobile" ? "text-[#e50914]" : "opacity-40")
+                (selected == "mobile" ? "text-white" : "opacity-40")
               }
             >
               <BsPhone className="text-2xl" />
@@ -251,7 +233,7 @@ const[selected,setSelected]=useState("basic");
             <div
               className={
                 "w-1/6  flex flex-col justify-start  text-center items-center " +
-                (selected == "basic" ? "text-[#e50914]" : "opacity-40")
+                (selected == "basic" ? "text-white" : "opacity-40")
               }
             >
               <BsPhone className="text-2xl mb-2" />
@@ -266,7 +248,7 @@ const[selected,setSelected]=useState("basic");
             <div
               className={
                 "w-1/6  flex flex-col justify-start  text-center items-center " +
-                (selected == "standard" ? "text-[#e50914]" : "opacity-40")
+                (selected == "standard" ? "text-white" : "opacity-40")
               }
             >
               <BsPhone className="text-2xl mb-2" />
@@ -281,7 +263,7 @@ const[selected,setSelected]=useState("basic");
             <div
               className={
                 "w-1/6  flex flex-col justify-start  text-center items-center " +
-                (selected == "premium" ? "text-[#e50914]" : "opacity-40")
+                (selected == "premium" ? "text-white" : "opacity-40")
               }
             >
               <BsPhone className="text-2xl mb-2" />
@@ -295,6 +277,23 @@ const[selected,setSelected]=useState("basic");
             </div>
           </div>
           <div className="w-100 h-[0.5px] bg-gray-400"></div>
+        </div>
+        <div className="container mt-10">
+          <p>
+            HD (720p), Full HD (1080p), Ultra HD (4K) and HDR availability
+            subject to your internet service and device capabilities. Not all
+            content is available in all resolutions.
+          </p>
+          <p>
+            See our Terms of Use for more details. Only people who live with you
+            may use your account. Watch on 4 different devices at the same time
+            with Premium, 2 with Standard, and 1 with Basic and Mobile.
+          </p>
+          <Link href="">
+            <a className="text-white mt-10 block text-2xl rounded-md bg-red-600 text-center w-full p-3">
+              Next
+            </a>
+          </Link>
         </div>
       </div>
     </div>
