857478fcf39fa8cc7d3629c2ea1060c537c3008d
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2023-05-14 00:54:18 +0530

    register route and register page done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,7 @@ import { SessionProvider } from "next-auth/react";
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Layout>
         <Component {...pageProps} />
-      </Layout>
     </SessionProvider>
   );
 }
