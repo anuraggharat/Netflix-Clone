8732df2e6849c9922d212c9e4f26db45fd59cc35
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2023-05-16 00:44:26 +0530

    removed import error

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth, { AuthOptions } from "next-auth/";
+import NextAuth, { AuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
