951f4dad8736f54cfa2e7e5ac3b1a2e53b337ef9
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2023-05-16 00:23:04 +0530

    updated version of next auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,4 +1,4 @@
-import NextAuth, { AuthOptions } from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth/";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import Credentials from "next-auth/providers/credentials";
@@ -6,7 +6,7 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { compare } from "bcrypt";
 import prismadb from "../../../lib/prismadb";
 
-export const authOptions: AuthOptions = {
+export const authOptions:AuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID || "",
