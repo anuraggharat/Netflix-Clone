5eb9d015d58a990a894e6c7f099ca0136a33c0f7
Anurag Gharat <53979540+anurag-gharat@users.noreply.github.com>
2023-05-16 00:14:28 +0530

    added types in app compenet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
 import '../styles/globals.css'
 import { SessionProvider } from "next-auth/react";
 import { Toaster } from 'react-hot-toast';
+import {AppProps} from 'next/app'
 
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }:AppProps) {
   return (
     <SessionProvider session={session}>
         <Component {...pageProps} />
